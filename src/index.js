650d0c670f9d95ee19b1028fda8cd429c6a93636
Masoud Aghaei <maghaie148@gmail.com>
2022-05-18 23:23:08 +0430

    first commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ContextApiRoutes from "./ContextApi/Routes";
+import ReduxRoutes from "./Redux/Routes";
+import Header from "./Header";
+import Home from "./Home";
+import ProviderRoutes from "./Provider/Routes";
+import ContextApiProviderRoutes from "./ContextApiProvider/Routes";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="ctx/*" element={<ContextApiRoutes />} />
+        <Route path="pvd" element={<ProviderRoutes />} />
+        <Route path="ctp/*" element={<ContextApiProviderRoutes />} />
+        <Route path="/rdx/*" element={<ReduxRoutes />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
